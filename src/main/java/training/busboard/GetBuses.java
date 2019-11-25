97898c1cb87b066c353aa075f35998ee537e5e0a
Tim Leach <Tim.Leach@softwire.com>
2019-11-25 16:25:53 +0000

    Part 3 (initially)

---
+++ src/main/java/training/busboard/GetBuses.java
@@ -0,0 +1,35 @@
+package training.busboard;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class GetBuses {
+    private int nStops;
+    private int nBuses;
+
+    public GetBuses(int nStops, int nBuses) {
+        this.nStops = nStops;
+        this.nBuses = nBuses;
+    }
+
+    public List<List<StopDataResponse>> getBusesNear(String postCode) {
+        QueryAPI queryAPI = new QueryAPI();
+        PostCodeResponse postCodeResponse = queryAPI.QueryPostCode(postCode);
+
+        // extract lat and long
+        float lat = postCodeResponse.result.latitude;
+        float lon = postCodeResponse.result.longitude;
+
+        RadiusResponse radiusResponse = queryAPI.QueryRadius(lat, lon);
+
+        return radiusResponse.stopPoints.stream().sorted(Comparator.comparingDouble(r->r.distance)).limit(nStops).map(stopData -> {
+            StopDataResponse[] stopDataResponses = queryAPI.QueryStop(stopData.naptanId);
+            Stream<StopDataResponse> busStream = Arrays.stream(stopDataResponses);
+            System.out.println("At bus stop " + stopDataResponses[0].stationName);
+            return busStream.sorted(Comparator.comparingInt(r -> r.timeToStation)).limit(nBuses).collect(Collectors.toList());
+        }).collect(Collectors.toList());
+    }
+}
--- src/main/java/training/busboard/web/BusInfo.java
+++ src/main/java/training/busboard/web/BusInfo.java
@@ -1,10 +1,25 @@
 package training.busboard.web;
 
+import training.busboard.GetBuses;
+import training.busboard.PostCodeResponse;
+import training.busboard.QueryAPI;
+import training.busboard.StopDataResponse;
+
+import java.util.List;
+import java.util.stream.Stream;
+
 public class BusInfo {
     private final String postcode;
+    private final List<List<StopDataResponse>> result;
+
+    public List<List<StopDataResponse>> getResult() {
+        return result;
+    }
 
     public BusInfo(String postcode) {
         this.postcode = postcode;
+        GetBuses getBuses = new GetBuses(2, 5);
+        this.result = getBuses.getBusesNear(postcode);
     }
 
     public String getPostcode() {
